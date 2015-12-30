a6c38584a4163139fa8ce35ad5f1bc8efe8f0bfd
kevinballon <kevinballon@gmail.com>
2015-12-29 21:49:52 -0500

    added css/js and a link

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// $(document).ready(function() {
+// });
+
+$('.image.royalty').click(function() {
+  // console.log('cargo');
+});
+
+$('.observation.shame').click(function() {
+  $('.image.shame').toggle();
+  $(this).animatescroll({padding:50});
+});
+
+$('.observation.royalty').click(function() {
+  $('.image.royalty').toggle();
+  $(this).animatescroll({padding:50});
+});
+
+$('.observation.cobras').click(function() {
+  $('.image.cobras').toggle();
+  $(this).animatescroll({padding:50});
+});
